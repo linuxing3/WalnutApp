4ab9ad65e8833d99a1fe82f4398c4714aa0b666a
linuxing3 <linuxing3@qq.com>
2022-11-30 03:22:31 -0400

    [bug] fonts not working

--- src/DemoLayer.h
+++ src/DemoLayer.h
@@ -1,11 +1,20 @@
 #include "Application.h"
+#include "Roboto-Regular.embed"
 #include "imgui.h"
 using Walnut::Layer;
 class DemoLayer : public Layer {
   bool m_ShowDemo = false;
+  ImFont *font;
 
 public:
-  virtual void OnAttach() override { m_ShowDemo = false; }
+  virtual void OnAttach() override {
+    m_ShowDemo = false;
+    ImFontConfig fontConfig;
+    ImGuiIO &io = ImGui::GetIO();
+    fontConfig.FontDataOwnedByAtlas = false;
+    font = io.Fonts->AddFontFromMemoryTTF(
+        (void *)g_RobotoRegular, sizeof(g_RobotoRegular), 20.0f, &fontConfig);
+  }
   virtual void OnUIRender() override {
     const char *version = ImGui::GetVersion();
     ImGui::Begin("Hello");
